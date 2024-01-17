4ec6e88faa370fe9a2d09b6884c6239916ba1214
ninq4 <nik.dolgov.05@mail.ru>
2024-01-17 16:52:52 +0300

    Пишу

--- task_2/index.php
+++ task_2/index.php
@@ -1,1 +1,11 @@
 <?php
+
+$count = 23;
+if ($count >= 18){
+    echo "Ему больше 18";
+}else if($count > 14){
+    echo "Ему больше 14";
+}https://github.com/ninq4/class-work.git
+else{
+    echo "Ему меньше 18";
+}
