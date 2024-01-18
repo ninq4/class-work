a57dcdede4cd71fb7500aeeef520fe31b3b39ba8
ninq4 <nik.dolgov.05@mail.ru>
2024-01-18 18:06:42 +0300

    Пофиксил роут

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -25,4 +25,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [\App\Http\Controllers\FrontendController::class, 'index']) -> name('home');
-Route::get('/', [\App\Http\Controllers\FrontendController::class, 'about']) -> name('about');
+Route::get('/about', [\App\Http\Controllers\FrontendController::class, 'about']) -> name('about');
