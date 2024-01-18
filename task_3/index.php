2dfc8ee956c85fbeb948064f639b73622d1c29c6
ninq4 <nik.dolgov.05@mail.ru>
2024-01-18 14:39:21 +0300

    Сделал первое задание(окончательно)

--- task_3/index.php
+++ task_3/index.php
@@ -1,6 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
+    $result = "";
 
+    for ($i = 0; $i <= 10; $i++) {
+        $result .= $i;
+        if($i != 10) {
+            $result .= "-";
+        }
+    }
 
-for($i = 0; $i <= 10; $i++) {
-    echo $i . '.';
-}
+    echo $result;
+
+?>
+</body>
+</html>
