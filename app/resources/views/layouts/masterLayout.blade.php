798e32e74936c3f1a51d058879a6efac95d91076
ninq4 <nik.dolgov.05@mail.ru>
2024-01-23 17:58:43 +0300

    Сделал папку Post с формами, index, edit, create

--- app/resources/views/layouts/masterLayout.blade.php
+++ app/resources/views/layouts/masterLayout.blade.php
@@ -7,6 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('/assets/style/main.css') }}">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <script defer src="{{asset('/assets/javascript/app.js')}}"></script>
 </head>
 <body>
@@ -14,5 +17,6 @@
 <main>
     @yield('content')
 </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
