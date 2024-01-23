404028b45b18406de98e8904356bb832cfb13d52
ninq4 <nik.dolgov.05@mail.ru>
2024-01-23 18:51:10 +0300

    Сделал роуты между формами, сделал PostController

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,3 +17,9 @@ Route::get('/', [\App\Http\Controllers\MainController::class, 'index']) -> name(
 Route::get('/about', [\App\Http\Controllers\MainController::class, 'about']) -> name('about');
 Route::get('/contacts', [\App\Http\Controllers\MainController::class, 'contacts']) -> name('contacts');
 Route::get('/portfolio', [\App\Http\Controllers\MainController::class, 'portfolio']) -> name('portfolio');
+
+Route::prefix('/post') -> group(function () {
+    Route::get('/index', [\App\Http\Controllers\PostController::class, 'index']) -> name('post.index');
+    Route::get('/create', [\App\Http\Controllers\PostController::class, 'create']) -> name('post.create');
+    Route::get('/edit', [\App\Http\Controllers\PostController::class, 'edit']) -> name('post.edit');
+});
